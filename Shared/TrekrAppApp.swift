9152d58912fd77918daad5f5d2e4b955f8159af9
lcs-eyencilek <90844648+lcs-eyencilek@users.noreply.github.com>
2022-01-18 09:29:44 -0500

    Initial Commit

---
+++ Shared/TrekrAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrAppApp.swift
+//  Shared
+//
+//  Created by Efe Yencilek on 2022-01-18.
+//
+
+import SwiftUI
+
+@main
+struct TrekrAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
